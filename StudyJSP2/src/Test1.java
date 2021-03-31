6d9bba293f38fd61af5e2bdbf30d557b65606578
unknown <kmj9953@hanmail.net>
2021-03-31 15:58:17 +0900

    jsp

---
+++ StudyJSP2/src/Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println(" 시작 ");
+	for(int n=10; n>=0; n--) {
+		for(long m = 0; m <10000000000L; m++);
+		System.out.println(n);
+		
+	}
+	
+		System.out.println(" 끝 ");
+		
+		
+		
+	}
+
+}
+
